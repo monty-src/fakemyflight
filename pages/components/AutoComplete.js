2f45d4ec887a8f85b2c1bb8c7eff585f21222134
monty-src <phoebe@montiers-mbp.lan>
2022-07-04 13:58:19 -0400

    Working on selection of dropdown

--- pages/components/AutoComplete.js
+++ pages/components/AutoComplete.js
@@ -1,10 +1,12 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, Children } from 'react';
 import classNames from 'classnames';
 
 const AutoComplete = () => {
     const ref = useRef();
+    const listRef = useRef();
     const [value, setValue] = useState('');
-    const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [select, setSelect] = useState(-1);
 
     const onChange = e => {
         if (e.target.value === '') {
@@ -19,13 +21,39 @@ const AutoComplete = () => {
     const onClick = e => {
         console.log('current target: ', e.target.textContent.trim());
         setValue(e.target.textContent.trim());
+        setIsMenuOpen(false);
     };
 
+    const count = 0;
+
     const keyDown = e => {
-      console.log('key:', e.key);
-      if (e.key = 'ArrowDown') {}
-      if (e.key = 'ArrowUp') {}
-      if (e.key = 'Enter') {}
+      switch(e.key) {
+        case 'ArrowDown':
+          if (isMenuOpen) {
+            if ( parseInt(select) === listRef.current.childNodes.length ) {
+              setSelect(parseInt(0));
+              return;
+            }
+            setSelect(parseInt(select) + 1);
+          }
+          console.log('select: ', select);
+          break;
+          case 'ArrowUp':
+            if ( parseInt(select) === 2) {
+              setSelect(parseInt(listRef.current.childNodes.length));
+              return;
+            } else if (parseInt(select) === -1) {
+              return;
+            }
+            setSelect(parseInt(select) - 1);
+            console.log('select: ', select);
+          break;
+          case 'Enter':
+            console.log('enter');
+            break;
+        default:
+          return;
+      }
     }
 
     useEffect(() => {
@@ -34,13 +62,11 @@ const AutoComplete = () => {
           setIsMenuOpen(false)
         }
       }
-  
-      document.addEventListener("mousedown", checkIfClickedOutside)
-  
+      document.addEventListener('mousedown', checkIfClickedOutside);
       return () => {
-        document.removeEventListener("mousedown", checkIfClickedOutside)
+        document.removeEventListener('mousedown', checkIfClickedOutside);
       }
-    }, [isMenuOpen])
+    }, [isMenuOpen]);
 
     return (
         <div className="relative" ref={ref}>
@@ -55,13 +81,15 @@ const AutoComplete = () => {
             className="w-full p-3 text-sm border-gray-200 rounded-lg" 
             id="fromAirport"/>
             {isMenuOpen &&
-              <div className="absolute z-10 bg-white w-full">
+              <div className="absolute z-10 bg-white w-full" ref={listRef}>
                 <div
+                  tabIndex="0"
                   onClick={onClick} 
                   className="border-b-2 p-2 border-gray-200 cursor-pointer">
                   Apple
                 </div>
                 <div
+                  tabIndex="0"
                   onClick={onClick}  
                   className="border-b-2 p-2 border-gray-200 cursor-pointer">
                   Banana
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import Head from 'next/head';
-import Image from 'next/image';
 
 import Form from './components/Form';
 import Navbar from './components/Navbar';
