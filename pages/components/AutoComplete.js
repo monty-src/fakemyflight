fbfffe3c7b6a455b109a8e3a70a59def56d1843b
monty-src <phoebe@montiers-mbp.lan>
2022-07-04 16:30:15 -0400

    finished dropdown autocomplete

--- pages/components/AutoComplete.js
+++ pages/components/AutoComplete.js
@@ -8,6 +8,17 @@ const AutoComplete = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [select, setSelect] = useState(-1);
 
+    const options = [
+        'Acerola',
+        'Apple',
+        'Apricots',
+        'Avocado',
+        'Banana',
+        'Blackberries',
+        'Blackcurrant',
+        'Blueberries'
+    ];
+
     const onChange = e => {
         if (e.target.value === '') {
           setIsMenuOpen(false);
@@ -30,36 +41,36 @@ const AutoComplete = () => {
       switch(e.key) {
         case 'ArrowDown':
           if (isMenuOpen) {
-            if ( parseInt(select) === listRef.current.childNodes.length ) {
-              setSelect(parseInt(0));
-              return;
+            const childrenLength = parseInt(listRef.current.childNodes.length) - 1;
+            if (childrenLength === parseInt(select)) {
+              setSelect(0);
+              return false;
             }
             setSelect(parseInt(select) + 1);
           }
-          console.log('select: ', select);
           break;
           case 'ArrowUp':
-            if ( parseInt(select) === 2) {
-              setSelect(parseInt(listRef.current.childNodes.length));
-              return;
-            } else if (parseInt(select) === -1) {
-              return;
+            const childrenLength = parseInt(listRef.current.childNodes.length) -1;
+            if ( parseInt(select) <= 0) {
+              setSelect(childrenLength);
+              return false;
             }
             setSelect(parseInt(select) - 1);
-            console.log('select: ', select);
           break;
           case 'Enter':
             console.log('enter');
+            listRef.current.childNodes[select].click();
             break;
         default:
           return;
       }
-    }
+    };
 
     useEffect(() => {
       const checkIfClickedOutside = e => {
         if (isMenuOpen && ref.current && !ref.current.contains(e.target)) {
-          setIsMenuOpen(false)
+          setIsMenuOpen(false);
+          setSelect(-1);
         }
       }
       document.addEventListener('mousedown', checkIfClickedOutside);
@@ -81,19 +92,18 @@ const AutoComplete = () => {
             className="w-full p-3 text-sm border-gray-200 rounded-lg" 
             id="fromAirport"/>
             {isMenuOpen &&
+              
               <div className="absolute z-10 bg-white w-full" ref={listRef}>
-                <div
-                  tabIndex="0"
-                  onClick={onClick} 
-                  className="border-b-2 p-2 border-gray-200 cursor-pointer">
-                  Apple
-                </div>
-                <div
-                  tabIndex="0"
-                  onClick={onClick}  
-                  className="border-b-2 p-2 border-gray-200 cursor-pointer">
-                  Banana
-                </div>
+                {options.map((option, idx) => 
+                  <div
+                    key={idx}
+                    onClick={onClick} 
+                    className={`p-2 border-gray-200 cursor-pointer ${(idx === select) ? 'border-blue-500 border-2' : 'border-b-2'}`}>
+                    {option}
+                  </div>
+                )}
+                
+                
               </div>}
         </div>
     )
