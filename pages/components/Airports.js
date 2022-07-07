f937092238f05b4eb1fe84e4871ef7d691f62f80
monty-src <phoebe@montiers-mbp.lan>
2022-07-07 08:22:55 -0400

    switch os

---
+++ pages/components/Airports.js
@@ -0,0 +1,142 @@
+
+const Airports = () => {
+
+
+  return (
+    <section className='
+        bg-[#10455a]
+        relative
+        py-6
+    '>
+      <div className='
+        flex
+        px-10
+        sm:px-20
+        whitespace-nowrap
+        space-x-10
+        sm:space-x-20
+        overflow-x-scroll
+        scrollbar-hide
+        text-white
+      '>
+          <h2 className='
+            cursor-pointer
+            transition
+            duration-100
+            transform
+            hover:scale-125
+            hover:text-white
+            active:text-red-500
+            last:pr-24'>Delta</h2>
+          <h2 className='
+            cursor-pointer
+            transition
+            duration-100
+            transform
+            hover:scale-125
+            hover:text-white
+            active:text-red-500
+            last:pr-24'>JetBlue</h2>
+          <h2 className='
+            cursor-pointer
+            transition
+            duration-100
+            transform
+            hover:scale-125
+            hover:text-white
+            active:text-red-500
+            last:pr-24'>American Airlines</h2>
+          <h2 className='
+            cursor-pointer
+            transition
+            duration-100
+            transform
+            hover:scale-125
+            hover:text-white
+            active:text-red-500
+            last:pr-24'>United Airlines</h2>
+          <h2 className='
+            cursor-pointer
+            transition
+            duration-100
+            transform
+            hover:scale-125
+            hover:text-white
+            active:text-red-500
+            last:pr-24'>Southwest Airlines</h2>
+          <h2 className='
+            cursor-pointer
+            transition
+            duration-100
+            transform
+            hover:scale-125
+            hover:text-white
+            active:text-red-500
+            last:pr-24'>JetBlue Airlines</h2>
+          <h2 className='
+            cursor-pointer
+            transition
+            duration-100
+            transform
+            hover:scale-125
+            hover:text-white
+            active:text-red-500
+            last:pr-24'>Frontier Airlines</h2>
+          <h2 className='
+            cursor-pointer
+            transition
+            duration-100
+            transform
+            hover:scale-125
+            hover:text-white
+            active:text-red-500
+            last:pr-24'>Skywest Airlines</h2>
+          <h2 className='
+            cursor-pointer
+            transition
+            duration-100
+            transform
+            hover:scale-125
+            hover:text-white
+            active:text-red-500
+            last:pr-24'>Spirit Airlines</h2>
+          <h2 className='
+            cursor-pointer
+            transition
+            duration-100
+            transform
+            hover:scale-125
+            hover:text-white
+            active:text-red-500
+            last:pr-24'>Alaska Airlines</h2>
+          <h2 className='
+            cursor-pointer
+            transition
+            duration-100
+            transform
+            hover:scale-125
+            hover:text-white
+            active:text-red-500
+            last:pr-24'>Hawaiian Airlines</h2>
+      </div>
+      <div className='
+        absolute
+        top-0
+        right-0
+        bg-gradient-to-l from-[#06202A]
+        h-20
+        w-1/12
+      '></div>
+      <div className='
+        absolute
+        top-0
+        left-0
+        bg-gradient-to-r from-[#06202A]
+        h-20
+        w-1/12
+      '></div>
+    </section>
+  )
+}
+
+export default Airports
