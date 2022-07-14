87efa2c46b7e796e1049ec02667a86d11fba98b9
0nesolo <01nesolo@gmail.com>
2022-07-14 10:49:46 -0400

    Updated changes to logo

--- components/Header.js
+++ components/Header.js
@@ -4,8 +4,8 @@ import Logo from '../public/logo.png';
 
 const Header = () => {
   return (
-    <header className="lg:col-span-5 p-10 grid grid-flow-col grid-cols-1 lg:grid-cols-5">
-      <div className="lg:col-start-1 lg:col-end-3 ">
+    <header className="lg:col-span-5 p-5 py-10 grid grid-flow-col grid-cols-1 lg:grid-cols-10">
+      <div className="lg:col-start-1 lg:col-end-4">
         <Image src={Logo} />
 
       </div>
