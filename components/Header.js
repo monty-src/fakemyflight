3a865005d7a9e0e2f727388e954b148213f941e0
0nesolo <01nesolo@gmail.com>
2022-07-14 09:58:13 -0400

    Added logo

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import Logo from '../public/logo.png';
 
 const Header = () => {
   return (
-    <header className="lg:col-span-5 p-5">
-        <h2 className="text-white text-2xl">Logo</h2>
+    <header className="lg:col-span-5 p-10 grid grid-flow-col grid-cols-1 lg:grid-cols-5">
+      <div className="lg:col-start-1 lg:col-end-3 ">
+        <Image src={Logo} />
+
+      </div>
     </header>
   )
 }
