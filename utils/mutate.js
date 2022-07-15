0ccdc83c619d4ceda832bcdecd60a1bb0d04ae7c
monty-src <phoebe@montiers-mbp.lan>
2022-07-15 05:49:54 -0400

    Re installed packages

---
+++ utils/mutate.js
@@ -0,0 +1,7 @@
+import JsonQuery from 'json-query';
+
+export const verboseAirline = () => {};
+export const verboseDurationMinutes = () => {};
+export const verboseStopOverMinutes = () => {};
+export const verboseDeparetureAirportCode = () => {};
+export const verboseArrivalAirportCode = () => {};
