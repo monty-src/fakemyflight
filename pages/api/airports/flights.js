91a359fbb9bf8d3fb5c89688971979ef1c397dd3
monty-src <phoebe@montiers-mbp.lan>
2022-07-15 06:35:51 -0400

    Dev added schema for request body flights

--- pages/api/airports/flights.js
+++ pages/api/airports/flights.js
@@ -2,12 +2,16 @@ import axios from 'axios';
 import moment from 'moment';
 import JsonQuery from 'json-query';
 
-import oneWay from '../one-way.json';
-import twoWay from '../two-way.json';
-import { toHoursAndMinutes } from '../../../utils/datetime-utils';
+import { validateFlightsRequest } from '../../../utils/schema';
 
 export default async function (req, res) {
-  const oneWayFlight = req.query.oneWay;
+  const { value: requestBody, error: requestSchemaError } =
+    validateFlightsRequest(req.body);
+  console.log('requestBody: ', requestBody);
+  console.log('requestSchemaError: ', requestSchemaError);
+  // console.log('request body: ', reqBody);
+
+  // const oneWayFlight = req.query.oneWay;
 
   // const { fromDate, toDate, radio, fromAirport, toAirport } = req.body;
   // const formattedFromAirport = fromAirport.slice(0, 3).toUpperCase();
@@ -25,77 +29,77 @@ export default async function (req, res) {
   // const { data } = response;
 
   // const { legs, trips, fares } = data;
-  const { legs, trips, fares, airlines, airports } =
-    oneWayFlight === 'true' ? oneWay : twoWay;
+  // const { legs, trips, fares, airlines, airports } =
+  //   oneWayFlight === 'true' ? oneWay : twoWay;
 
-  const flightInformationFormatted =
-    oneWayFlight === 'true'
-      ? trips.map(({ id, legIds }) => {
-          const leg = JsonQuery(`legs[id=${legIds[0]}]`, {
-            data: { legs },
-          }).value;
-          const { value: fare } = JsonQuery(`fares[tripId=${id}]`, {
-            data: { fares },
-          });
-          leg.segments = leg.segments.map((segment) => {
-            const {
-              airlineCode,
-              durationMinutes,
-              stopoverDurationMinutes,
-              departureAirportCode,
-              arrivalAirportCode,
-            } = segment;
-            const verboseAirline = JsonQuery(`airlines[code=${airlineCode}]`, {
-              data: { airlines },
-            }).value.name;
-            const verboseDurationMinutes = toHoursAndMinutes(durationMinutes);
-            const verboseStopOverDurationMinutes = toHoursAndMinutes(
-              stopoverDurationMinutes
-            );
-            const verboseDepartureAirportCode = JsonQuery(
-              `airports[code=${departureAirportCode}]`,
-              { data: { airports } }
-            ).value.name;
-            const verboseArrivalAirportCode = JsonQuery(
-              `airports[code=${arrivalAirportCode}]`,
-              { data: { airports } }
-            ).value.name;
-            return {
-              ...segment,
-              verboseAirline,
-              verboseDurationMinutes,
-              verboseStopOverDurationMinutes,
-              verboseDepartureAirportCode,
-              verboseArrivalAirportCode,
-            };
-          });
-          return { leg, fare };
-        })
-      : trips.map(({ id, legIds }) => {
-          const leg = JsonQuery(`legs[id=${legIds[0]}]`, {
-            data: { legs },
-          }).value;
-          const second_leg = JsonQuery(`legs[id=${legIds[1]}]`, {
-            data: { legs },
-          }).value;
-          const { value: fare } = JsonQuery(`fares[tripId=${id}]`, {
-            data: { fares },
-          });
-          return { leg, second_leg, fare };
-        });
+  // const flightInformationFormatted =
+  //   oneWayFlight === 'true'
+  //     ? trips.map(({ id, legIds }) => {
+  //         const leg = JsonQuery(`legs[id=${legIds[0]}]`, {
+  //           data: { legs },
+  //         }).value;
+  //         const { value: fare } = JsonQuery(`fares[tripId=${id}]`, {
+  //           data: { fares },
+  //         });
+  //         leg.segments = leg.segments.map((segment) => {
+  //           const {
+  //             airlineCode,
+  //             durationMinutes,
+  //             stopoverDurationMinutes,
+  //             departureAirportCode,
+  //             arrivalAirportCode,
+  //           } = segment;
+  //           const verboseAirline = JsonQuery(`airlines[code=${airlineCode}]`, {
+  //             data: { airlines },
+  //           }).value.name;
+  //           const verboseDurationMinutes = toHoursAndMinutes(durationMinutes);
+  //           const verboseStopOverDurationMinutes = toHoursAndMinutes(
+  //             stopoverDurationMinutes
+  //           );
+  //           const verboseDepartureAirportCode = JsonQuery(
+  //             `airports[code=${departureAirportCode}]`,
+  //             { data: { airports } }
+  //           ).value.name;
+  //           const verboseArrivalAirportCode = JsonQuery(
+  //             `airports[code=${arrivalAirportCode}]`,
+  //             { data: { airports } }
+  //           ).value.name;
+  //           return {
+  //             ...segment,
+  //             verboseAirline,
+  //             verboseDurationMinutes,
+  //             verboseStopOverDurationMinutes,
+  //             verboseDepartureAirportCode,
+  //             verboseArrivalAirportCode,
+  //           };
+  //         });
+  //         return { leg, fare };
+  //       })
+  //     : trips.map(({ id, legIds }) => {
+  //         const leg = JsonQuery(`legs[id=${legIds[0]}]`, {
+  //           data: { legs },
+  //         }).value;
+  //         const second_leg = JsonQuery(`legs[id=${legIds[1]}]`, {
+  //           data: { legs },
+  //         }).value;
+  //         const { value: fare } = JsonQuery(`fares[tripId=${id}]`, {
+  //           data: { fares },
+  //         });
+  //         return { leg, second_leg, fare };
+  //       });
 
-  if (oneWayFlight === 'true') {
-    flightInformationFormatted.sort(
-      (a, b) => a.leg.durationMinutes - b.leg.durationMinutes
-    );
-  } else {
-    flightInformationFormatted.sort(
-      (a, b) =>
-        a.leg.durationMinutes +
-        a.second_leg.durationMinutes -
-        (b.leg.durationMinutes + b.second_leg.durationMinutes)
-    );
-  }
+  // if (oneWayFlight === 'true') {
+  //   flightInformationFormatted.sort(
+  //     (a, b) => a.leg.durationMinutes - b.leg.durationMinutes
+  //   );
+  // } else {
+  //   flightInformationFormatted.sort(
+  //     (a, b) =>
+  //       a.leg.durationMinutes +
+  //       a.second_leg.durationMinutes -
+  //       (b.leg.durationMinutes + b.second_leg.durationMinutes)
+  //   );
+  // }
 
-  return res.status(200).json(flightInformationFormatted);
+  // return res.status(200).json(flightInformationFormatted);
 }
--- utils/schema.js
+++ utils/schema.js
@@ -1,7 +1,30 @@
-import JsonQuery from 'json-query';
+import Joi from 'joi';
+import moment from 'moment';
 
-export const verboseAirline = () => {};
-export const verboseDurationMinutes = () => {};
-export const verboseStopOverMinutes = () => {};
-export const verboseDeparetureAirportCode = () => {};
-export const verboseArrivalAirportCode = () => {};
+const extractAiport = (airport) => airport.slice(0, 3).toUpperCase();
+
+export const validateFlightsRequest = (data) => {
+  let { fromDate, toDate, radio, fromAirport, toAirport } = data;
+
+  const todaysDate = moment().format('YYYY-MM-DD');
+  fromDate = moment(fromDate).format('YYYY-MM-DD');
+  toDate = radio === 1 ? moment(toDate).format('YYYY-MM-DD') : null;
+  fromAirport = extractAiport(fromAirport);
+  toAirport = extractAiport(toAirport);
+
+  const schema = Joi.object({
+    fromDate: Joi.date().min(todaysDate).required(),
+    toDate: [Joi.date().greater(Joi.ref('from')).required(), Joi.allow(null)],
+    radio: [Joi.number().valid(0).required(), Joi.number().valid(1).required()],
+    fromAirport: Joi.string().min(2).max(3).required(),
+    toAirport: Joi.string().min(2).max(3).required(),
+  });
+
+  return schema.validate({
+    fromDate,
+    toDate,
+    radio,
+    fromAirport,
+    toAirport,
+  });
+};
